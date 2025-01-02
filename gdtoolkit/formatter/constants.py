9a17b6815f6c09df6427b893646a5253c56cefd8
ludamad <adam.domurad@gmail.com>
2025-01-02 15:39:04 -0500

    monmon specifics setting for empty lines

--- gdtoolkit/formatter/constants.py
+++ gdtoolkit/formatter/constants.py
@@ -19,14 +19,16 @@ DEFAULT_SURROUNDING_EMPTY_LINES_TABLE = MappingProxyType(
 )
 GLOBAL_SCOPE_SURROUNDING_EMPTY_LINES_TABLE = MappingProxyType(
     {
-        "class_def": 2,
-        "func_def": 2,
-        "static_func_def": 2,
-        "remote_func_def": 2,
-        "remotesync_func_def": 2,
-        "master_func_def": 2,
-        "mastersync_func_def": 2,
-        "puppet_func_def": 2,
-        "sync_func_def": 2,
+        # <MONMON EDIT>
+        "class_def": 1,
+        "func_def": 1,
+        "static_func_def": 1,
+        "remote_func_def": 1,
+        "remotesync_func_def": 1,
+        "master_func_def": 1,
+        "mastersync_func_def": 1,
+        "puppet_func_def": 1,
+        "sync_func_def": 1,
+        # </MONMON EDIT>
     }
 )
